fbd980200db089a58b1cdb5bf875de54a41bb881
ByeongRyul Cho <ryullulala@icloud.com>
2022-04-29 18:00:22 +0900

    listTodos queries

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { API } from "aws-amplify";
+import { List } from "antd";
+import "antd/dist/antd.min.css";
+import { listTodos } from "./graphql/queries";
+import { useEffect, useReducer } from "react";
+
+const initialState = {
+  notes: [],
+  loading: true,
+  error: false,
+  form: { name: "", description: "" },
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "SET_NOTES":
+      return { ...state, notes: action.notes, loading: false };
+    case "ERROR":
+      return { ...state, loading: false, error: true };
+    default:
+      return state;
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  async function fetchNotes() {
+    try {
+      const notesData = await API.graphql({
+        query: listTodos,
+      });
+      dispatch({ type: "SET_NOTES", notes: notesData.data.listTodos.items });
+    } catch (err) {
+      console.log("error: ", err);
+      dispatch({ type: "ERROR" });
+    }
+  }
+
+  function renderItem(item) {
+    return (
+      <List.Item style={styles.item}>
+        <List.Item.Meta title={item.name} description={item.description} />
+      </List.Item>
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.container}>
+      <List
+        loading={state.loading}
+        dataSource={state.notes}
+        renderItem={renderItem}
+      />
     </div>
   );
 }
+const styles = {
+  container: { padding: 20 },
+  input: { marginBottom: 10 },
+  item: { textAlign: "left" },
+  p: { color: "#1890ff" },
+};
 
 export default App;
